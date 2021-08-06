9d4f898aa0fb65cc1cd164a657d02067cc06069a
garvitgupta13 <garvitgupta2001@gmail.com>
2021-08-06 19:41:48 +0530

    adding orders and product table to accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Customers
+from .models import *
 
+#to show Customer table to django admin page
 admin.site.register(Customers)
+admin.site.register(Product)
+admin.site.register(Order)
