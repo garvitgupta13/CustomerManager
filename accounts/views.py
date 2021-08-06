2ba9bb28af8ea62da4cd5c11fa78d81c1251ad71
garvitgupta13 <garvitgupta2001@gmail.com>
2021-08-06 13:18:10 +0530

    rendering html templates

--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return  HttpResponse('Home')
-
-def products(request):
-    return HttpResponse('Products')
+    return  render(request,'accounts/dashboard.html')
 
 def customer(request):
-    return HttpResponse('Customer')
+    return render(request,'accounts/customers.html')
+
+def products(request):
+    return render(request,'accounts/products.html')
