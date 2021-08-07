29b3dad5cc03a33ac35ff67f74d61c3d581ccdbb
garvitgupta13 <garvitgupta2001@gmail.com>
2021-08-07 19:22:19 +0530

    Adding links to redirect to corresponding page

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/',views.products),
-    path('customer/<str:custId>',views.customer)
+    path('', views.home,name="home"),
+    path('products/',views.products,name="products"),
+    path('customer/<str:custId>',views.customer,name="customer")#instead of calling the link we name it as 'customer
 ]
