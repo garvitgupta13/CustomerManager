6f8aaff7e736fe0a1d5f1f5788c11a3ec2ed10d4
garvitgupta13 <garvitgupta2001@gmail.com>
2021-08-08 23:17:01 +0530

    Adding signup form for user

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import  forms
+
 from .models import Order
 
 class OrderForm(ModelForm):
     class Meta:
         model=Order
         fields='__all__' #take all the attributes of Order class present in models.py i.e ["customer","product","date_created","status"]
+
+# CreateUserForm class will inherit the prebuilt class 'UserCreationFrom'
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','email','password1','password2']
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+
     path('', views.home,name="home"),
     path('products/',views.products,name="products"),
     path('customer/<str:custId>',views.customer,name="customer"),#instead of calling the link we name it as 'customer
+
     path('create_order/<str:custId>',views.createOrder, name="create_order"),
     path('update_order/<str:orderId>',views.updateOrder, name="update_order"),
     path('delete_order/<str:orderId>',views.deleteOrder, name="delete_order")
