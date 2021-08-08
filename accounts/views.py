c3f0ad7a5f2b67bbae5a576f6f6ed6e198706f28
garvitgupta13 <garvitgupta2001@gmail.com>
2021-08-08 23:27:26 +0530

    Adding signin form for user

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
+from django.contrib.auth import authenticate, login, logout
+
 from .models import *
 from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
@@ -24,6 +26,18 @@ def registerPage(request):
     return render(request,'accounts/register.html',context)
 
 def loginPage(request):
+    if(request.method=="POST"):
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, "Username or password is incorrect")
+
     context={}
     return render(request,'accounts/login.html',context)
 
