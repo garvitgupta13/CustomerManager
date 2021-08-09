64b2e251d63ae41e9ffdda2c73c9aa0698e1641d
garvitgupta13 <garvitgupta2001@gmail.com>
2021-08-09 18:03:27 +0530

    Initializing user profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
 
     path('', views.home,name="home"),
+    path('user/',views.userPage, name="user"),
     path('products/',views.products,name="products"),
     path('customer/<str:custId>',views.customer,name="customer"),#instead of calling the link we name it as 'customer
 
